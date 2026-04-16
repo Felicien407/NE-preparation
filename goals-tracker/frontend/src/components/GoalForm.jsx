6b7ea8f8a6da4f2679f8a968c438580db23341e1
Felicien407 <felicien@gmail.com>
2026-04-16 16:56:37 +0200

    Done with goals frontend

--- goals-tracker/frontend/src/components/GoalForm.jsx
+++ goals-tracker/frontend/src/components/GoalForm.jsx
@@ -1,37 +1,34 @@
-import { useState } from "react"
-import { useDispatch } from "react-redux"
-import { createGoal } from "../features/goals/goalSlice.js"
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createGoal } from '../features/goals/goalSlice'
 
-const GoalForm = () => {
-  const [text, setText] = useState("")
+function GoalForm() {
+  const [text, setText] = useState('')
 
   const dispatch = useDispatch()
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault()
 
     dispatch(createGoal({ text }))
-    setText("")
+    setText('')
   }
+
   return (
-    <section className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="text">Goal</label>
+    <section className='form'>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='text'>Goal</label>
           <input
-            type="text"
-            name="text"
-            id="text"
+            type='text'
+            name='text'
+            id='text'
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
         </div>
-
-        <div className="form-group">
-          <button
-            type="submit"
-            className="btn btn-block"
-          >
+        <div className='form-group'>
+          <button className='btn btn-block' type='submit'>
             Add Goal
           </button>
         </div>
--- goals-tracker/frontend/src/pages/Dashboard.jsx
+++ goals-tracker/frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,19 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import { useSelector, useDispatch } from "react-redux"
-import GoalForm from "../components/GoalForm"
-import Spinner from "../components/Spinner"
-import { getGoals, reset } from "../features/goals/goalSlice"
-import GoalItem from "../components/GoalItem"
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const { user } = useSelector((state) => state.auth)
-  const { goals, isLoading, isError, message } = useSelector((state) => state.goals)
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  )
 
   useEffect(() => {
     if (isError) {
@@ -21,31 +23,33 @@ function Dashboard() {
     if (!user) {
       navigate('/login')
     }
-    dispatch(getGoals(goals))
+
+    dispatch(getGoals())
 
     return () => {
       dispatch(reset())
     }
-  }, [user, navigate, dispatch, isError, message])
+  }, [user, navigate, isError, message, dispatch])
 
   if (isLoading) {
-    <Spinner />
+    return <Spinner />
   }
+
   return (
     <>
-      <section className="heading">
+      <section className='heading'>
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
 
       <GoalForm />
 
-      <section className="content">
+      <section className='content'>
         {goals.length > 0 ? (
-          <div className="goals">
-            {goals.map((goal) => {
-              <GoalItem key={goal.id} goal={goal} />
-            })}
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
           </div>
         ) : (
           <h3>You have not set any goals</h3>
